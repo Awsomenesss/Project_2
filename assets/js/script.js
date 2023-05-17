c47caa75d7b0e8ac9288fda75a9f8c9ecea13456
Awsomenesss <>
2023-05-17 08:10:11 +0000

    pushing changes to github

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,152 @@
-//get the button elements and add event listener to them
-const choice = document.getElementsByClassName("far");
-const computerScore=document.getElementsByTagName("choice");
-var computerChoiceDisplay = document.getElementById("computer_choice");
-var userChoiceDisplay = document.getElementById("player_choice");
-var resultDisplay = document.getElementById("result_display");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const lizard = document.getElementById("lizard");
+const spock = document.getElementById("spock");
+const resetBtn = document.getElementById('reset');
+const incrementBtn = document.getElementById('incrementBtn');
+const decrementBtn = document.getElementById('decrementBtn');
+const maxClicksElement = document.getElementById('maxClicks');
 
-var userScore = 0;
+let result = "";
+let playerChoice = "";
+let compChoice = "";
+let choices = ["rock", "paper", "scissors", "lizard", "spock"];
+let maxClicks = 1;
+let currentClicks = 0;
+let gameEnded = false;
 
-let computerLabel = document.getElementById("computer_label");
-let playerLabel = document.getElementById("player_label");
-
-
-var result;
-
-
-
-    document.addEventListener("DOMContentLoaded", function() {
-        let buttons = document.getElementsByTagName("choice");
-    
-        for (let button of buttons) {
-            button.addEventListener("click", function() {
-                let playerChoice = this.getAttribute("choice");
-                playGame (playerChoice);
-        });
+rock.addEventListener("click", function () {
+    if (!gameEnded) {
+        whoWins("Rock");
     }
 });
-console.log(choice)
+paper.addEventListener("click", function () {
+    if (!gameEnded) {
+        whoWins("Paper");
+    }
+});
+scissors.addEventListener("click", function () {
+    if (!gameEnded) {
+        whoWins("Scissors");
+    }
+});
+lizard.addEventListener("click", function () {
+    if (!gameEnded) {
+        whoWins("Lizard");
+    }
+});
+spock.addEventListener("click", function () {
+    if (!gameEnded) {
+        whoWins("Spock");
+    }
+});
 
+incrementBtn.addEventListener('click', function () {
+    if (maxClicks < 10) {
+        maxClicks++;
+        maxClicksElement.innerText = maxClicks;
+    }
+});
 
+decrementBtn.addEventListener('click', function () {
+    if (maxClicks > 1) {
+        maxClicks--;
+        maxClicksElement.innerText = maxClicks;
+    }
+});
+
+function whoWins(playerChoice) {
+    compChoice = decideCompChoice();
+    playerChoice = playerChoice.toLowerCase();
+
+    if (currentClicks >= maxClicks) {
+        determineResult();
+    } else {
+        if (playerChoice === compChoice) {
+            result = "Draw!";
+        } else {
+            if (playerChoice === "rock" && (compChoice === "scissors" || compChoice === "lizard")) {
+                result = "Win!";
+            } else if (playerChoice === "paper" && (compChoice === "rock" || compChoice === "spock")) {
+                result = "Win!";
+            } else if (playerChoice === "scissors" && (compChoice === "paper" || compChoice === "lizard")) {
+                result = "Win!";
+            } else if (playerChoice === "lizard" && (compChoice === "spock" || compChoice === "paper")) {
+                result = "Win!";
+            } else if (playerChoice === "spock" && (compChoice === "scissors" || compChoice === "rock")) {
+                result = "Win!";
+            } else {
+                result = "Loss!";
+            }
+        }
+        currentClicks++;
+        results(result, playerChoice, compChoice);
+    }
+}
+
+function decideCompChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function results(result, playerChoice, compChoice) {
+    if (result === "Win!") {
+        playerScore();
+    } else if (result === "Loss!") {
+        compScore();
+    }
+
+    showPlayerScore(playerChoice);
+    showCompScore(compChoice);
+
+    if (currentClicks >= maxClicks) {
+        determineResult();
+    } else {
+        result = "";
+    }
+}
+
+function showPlayerScore(playerChoice) {
+    document.getElementById("players-choice").innerHTML = `<button class="btn ${playerChoice.toLowerCase()}"><i class="far fa-hand-${playerChoice.toLowerCase()}"></i></button>`;
+}
+
+function showCompScore(compChoice) {
+    document.getElementById("comp-choice").innerHTML = `<button class="btn ${compChoice.toLowerCase()}"><i class="far fa-hand-${compChoice.toLowerCase()}"></i></button>`;
+}
     
+    function playerScore() {
+    let playerScore = parseInt(document.getElementById("wins").innerText);
+    playerScore++;
+    document.getElementById("wins").innerText = playerScore;
+    }
     
-
-
-function selectChoice(){
-    you = this.id;
-    document.getElementById("userScore").src = innerHTML + "far";}
-
+    function compScore() {
+    let compScore = parseInt(document.getElementById("losses").innerText);
+    compScore++;
+    document.getElementById("losses").innerText = compScore;
+    }
     
-
-// Generates computer choice
-
-
-// Compares user and computer choice and calculates a winner
-
-
-// Gets current computer score and increments by 1
-function incrementComputerScore() {
-    // Gets the current computer score from the DOM and increments it by 1
-    let oldScore = computerScore.innerText;
-    computerScore.innerText = ++oldScore;
-    let newScore = computerScore.innerHTML;}
- 
-
-// Gets current user score and increments 1
-function incrementUserScore() {
-    // Gets the current player score from the DOM and increments it by 1
-    let oldScore = userScore.innerText;
-    userScore.innerText = ++oldScore;
-    let newScore = userScore.innerHTML;}
+    function determineResult() {
+    let playerScore = parseInt(document.getElementById("wins").innerText);
+    let compScore = parseInt(document.getElementById("losses").innerText);
+    if (playerScore > compScore) {
+    result = "You Win!";
+    } else if (playerScore < compScore) {
+    result = "You Lose!";
+    } else {
+    result = "It's a Draw!";
+    }
+    document.getElementById("result").innerText = result;
+    gameEnded = true;
+    currentClicks = 0;
+    }
+    
+    resetBtn.addEventListener('click', function () {
+    document.getElementById("wins").innerText = '0';
+    document.getElementById("losses").innerText = '0';
+    document.getElementById("result").innerText = '';
+    gameEnded = false;
+    currentClicks = 0;
+    });
+    
+    maxClicksElement.innerText = maxClicks;
